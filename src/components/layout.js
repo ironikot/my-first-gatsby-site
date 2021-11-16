1e574491594ea9e42ff372095837c7a19daa183c
Tokinori Shimodaira <tokinorishimodaira@s-tokiMacBook-Pro.local>
2021-11-16 22:52:28 +0900

    add stle

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,24 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container } from './layout.module.css'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLintText
+ } from './layout.module.css'
 const Layout = ({pageTitele, children}) =>{
     return(
         <div className={container}>
             <title>{pageTitele}</title>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to="/" className={navLintText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLintText}>About</Link></li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitele}</h1>
+                <h1 className={heading}>{pageTitele}</h1>
                 {children}
             </main>
         </div>
